ec180fb84da1116d7ff542e1ac77b8ead971ddf3
Nikka-Sher <npiterman@gmail.com>
2017-06-14 22:03:22 -0700

    5 second notification

--- NewNotifications/ViewController.swift
+++ NewNotifications/ViewController.swift
@@ -31,10 +31,41 @@ class ViewController: UIViewController {
     
     
     @IBAction func notifyButtonTapped(sender: UIButton) {
+    
+        
+        scheduleNotification(inSeconds: 5, completion: { success in
+            if success {
+                print ("Successfully scheduled notification")
+                
+            } else {
+                print("Error scheduling notification")
+            }
+            })
+        
    
     }
     
-    func scheduleNotification(inSeconds: TimeInterval, completion: (_ Success: Bool) -> ()) {
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        let notif = UNMutableNotificationContent()
+        
+        notif.title = "New Notification"
+        notif.subtitle = "These are great!"
+        notif.body = "The new notification options in iOS 10 are what I've always dreamed of!"
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+            print(error ?? "")
+            completion(false)
+            
+            } else {
+                completion(true)
+            }
+            })
         
     }
     
